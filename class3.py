9967d8c7227279b572d3ec74721533912034dc4f
suppakitti <suppakitti.derm@mail.kmutt.ac.th>
2025-07-23 11:16:42 +0700

    add class3

---
+++ class3.py
@@ -0,0 +1,55 @@
+# haverice = True
+# havespoon = False
+# havehand = True
+
+# if haverice:
+#     if havespoon:
+#         print("กินข้าว")
+#     elif haverice:
+#         print("กินข้าวเหนียว")
+
+
+# score = int(input("Enter your score :"))
+
+# if score >=50:
+#     if score <= 100:
+#         if score >= 80:
+#             print("A")
+#         if score >= 70:
+#             if score <80:
+#                 print("B")
+#         if score  >= 60:
+#             if score < 70:
+#                 print("C")
+#         if score >=50:
+#             if score < 60:
+#                 print("D")
+
+
+# for i in range(1,5):
+#     print(i)
+#     print("สวัสดี")
+
+# i = 0
+# while i <8:
+#     print(i)
+#     i +=1
+
+
+# while True:
+#     choice = int(input("กรอก 1 เพื่อบวกเลข,กรอก 2 เพื่อออก"))
+#     if choice == 1:
+#         num = int(input("เลขที่ต้องการจะบวก"))
+#         sum = 0
+#         for i in range(num):
+#             num1 = int(input("กรอกเลข"))
+#             sum = sum + num1
+#         print(sum)
+#     if choice == 2:
+#         break
+
+
+
+
+        
+    
