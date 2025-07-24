101d39ca9400ea8c0915e6bdcd3008e5eace9e84
suppakitti <suppakitti.derm@mail.kmutt.ac.th>
2025-07-24 12:00:34 +0700

    class4

---
+++ class4.py
@@ -0,0 +1,66 @@
+# def grade(score):
+#     if score >= 80:
+#         print("A")
+#     elif score >= 70:
+#         print("B")
+#     elif score >= 60:
+#         print("C")
+#     else:
+#         print("D")
+# grade(78)
+
+
+# def MY_some(a,b):
+#     return a+b
+# result = MY_some(5,5)
+# if result ==10:
+#     print("GOOD")
+
+
+
+# def menu():
+#     while True:
+#         Menu = int(input("1.เลือกคำนวณราคาสินค้า 2.ออก"))
+#         if Menu ==1:
+#             quantity = float(input("กรอกจำนวนสินค้า : "))
+#             price = float(input("กรอกราคา : "))
+#             print(calculate(quantity,price))
+#         else:
+#             print("ออก")
+#             break
+
+# def calculate(quantity,price):
+#      sum = (quantity * price)*1.07
+#      return sum
+
+# menu()
+
+
+# MY_list = ["apple",1,2.5]
+
+# for i in MY_list:
+#     print(i)
+
+
+# MY_dict = {"myname":"milan","age": 19}
+# MY_dict["age"] = 30 #เพิ่มค่าลงไป "":""
+
+# print(MY_dict)
+
+
+# student = [
+#     {"name":"milan","id":1},
+#     {"name":"kao","id":2},
+#     {"name":"stamp","id":3}
+# ]
+
+# for i in student:
+#     print(i["name"])
+
+
+
+
+
+
+
+
